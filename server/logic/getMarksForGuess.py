af847bff0268ff3a574bc000171a9a762a4f860b
yaniv-cohen <yaniv.fast@gmail.com>
2023-05-05 00:26:42 +0300

    fixed marks logic

--- server/logic/getMarksForGuess.py
+++ server/logic/getMarksForGuess.py
@@ -6,17 +6,18 @@ def getMarksForGuess(word1, word2):
         char=word1[i]
         if(char == word2[i]):
             outputBlack+=1
-            word2[i]=""
-            word1[i]=""
+            # word2[i]=""
+            # word1[i]=""
         i+=1
     i=0
     outputWhite = 0
     checked = ['']
     while(i<len(word1)):
+        char = word1[i]
         if(checked.count(word1[i])==0):
-            outputWhite+= word2.count(word1[i])
-            checked.append(word1[i])
+            outputWhite += min(word1.count(char), word2.count(char))
+            checked.append(char)
         i+=1
         
     
-    return [outputBlack, outputWhite]
+    return [outputBlack, outputWhite -outputBlack]
