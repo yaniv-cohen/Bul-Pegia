3d4685d2da44427c1da850e3cb24bd2e4fea9ce8
yaniv-cohen <yaniv.fast@gmail.com>
2023-06-09 20:47:38 +0300

    removed mongo

--- src/components/utils/ToggleButton.tsx
+++ src/components/utils/ToggleButton.tsx
@@ -1,5 +1,3 @@
-import { IsAny } from "mongodb";
-
 export const ToggleButton = ({ fn, text, children }: { fn: Function, text: string, children?: any }) => {
     return <button style={{ "padding": "10px" }} onClick={() => fn()}>{text}</button>;
 };
